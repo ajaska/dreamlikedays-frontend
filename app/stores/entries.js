d2a084063de2c2b6c955d42a772f887d4974fa9c
Arlan Jaska <ajaska@berkeley.edu>
2015-08-04 01:53:16 -0700

    Fix adding entries

--- app/stores/entries.js
+++ app/stores/entries.js
@@ -5,8 +5,9 @@ const initialState = [];
 export default function entries(state = initialState, action) {
   switch (action.type) {
     case ADD_ENTRY:
+      // FIXME: finding the largest id is a bit of a hack right now
       return [{
-        id: (state.length === 0) ? 0 : state[0].id + 1,
+        id: (state.length === 0) ? 0 : Math.max(state[0].id, state[state.length-1].id) + 1,
         title: action.title,
         body: action.body
       }, ...state];
